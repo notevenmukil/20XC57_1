47deca91c40fdee94c9aa363865376d935914683
Mukil <81077219+notevenmukil@users.noreply.github.com>
2022-07-15 11:44:18 +0530

    Update script1.js

--- script1.js
+++ script1.js
@@ -14,10 +14,10 @@ function display(){
 }
 
 function setCookie(){
-	/*const d = new Date();
+	const d = new Date();
 	d.setTime(d.getTime() + (exdays*24*60*60*1000));
 	let expires = "expires="+d.toUTCString();
-	document.cookie = cname+"="+cvalue+";"+ expires +";path=/";*/
-	document.cookie = "username=John Doe"; 
+	document.cookie = cname+"="+cvalue+";"+ expires +";path=/";
+	//document.cookie = "username=John Doe"; 
 	console.log("here");
 }
